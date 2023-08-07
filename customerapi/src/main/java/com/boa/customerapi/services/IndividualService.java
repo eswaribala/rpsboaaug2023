26ac01d6421ebe49f0d8955bcc43d11c65686dcf
eswaribala <parameswaribala@gmail.com>
2023-08-07 14:26:28 +0530

    updated

--- customerapi/src/main/java/com/boa/customerapi/services/IndividualService.java
+++ customerapi/src/main/java/com/boa/customerapi/services/IndividualService.java
@@ -5,12 +5,21 @@ import com.boa.customerapi.repositories.IndividualRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.AbstractQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Service
 public class IndividualService {
     @Autowired
     private IndividualRepo individualRepo;
+
+    @Autowired
+    private EntityManager entityManager;
 
     //add
 
@@ -28,6 +37,26 @@ public class IndividualService {
 
     public Individual getIndividualById(long customerId){
         return this.individualRepo.findById(customerId).orElse(null);
+    }
+
+
+    public List<Individual> getIndividualByContactNo(long contactNo){
+        return this.individualRepo.findByContactNo(contactNo);
+    }
+
+
+    public List<Individual> getIndividualByFirstName(String firstName){
+
+        CriteriaBuilder cb= entityManager.getCriteriaBuilder();
+        AbstractQuery<Individual> cq=cb.createQuery(Individual.class);
+
+        Root<Individual> individualObject=cq.from(Individual.class);
+        cq.where(cb.equal(individualObject.get("name").<String> get("firstName") ,firstName));
+
+        CriteriaQuery<Individual> selectResult=((CriteriaQuery<Individual>)cq).select(individualObject);
+        TypedQuery<Individual> tq=entityManager.createQuery(selectResult);
+        return tq.getResultList();
+
     }
 
     //update
@@ -52,7 +81,7 @@ public class IndividualService {
 
         if(individual!=null){
             this.individualRepo.deleteById(customerId);
-            
+
             status=true;
 
             }
