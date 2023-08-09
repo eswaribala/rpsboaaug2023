dd32b42dc8197114343df48dcb8dc628ca8ad838
eswaribala <parameswaribala@gmail.com>
2023-08-09 15:51:20 +0530

    updated

--- jwtsecurity/src/main/java/com/boa/jwtsecurity/filters/JwtAuthenticationFilter.java
+++ jwtsecurity/src/main/java/com/boa/jwtsecurity/filters/JwtAuthenticationFilter.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.boa.jwtsecurity.models.User;
 
 
