71aed8bde59202e2afee45444c85cdabfdcbeb85
eswaribala <parameswaribala@gmail.com>
2023-08-09 15:45:59 +0530

    updated

--- jwtsecurity/src/main/java/com/boa/jwtsecurity/filters/JwtAuthenticationFilter.java
+++ jwtsecurity/src/main/java/com/boa/jwtsecurity/filters/JwtAuthenticationFilter.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-
+import com.boa.jwtsecurity.models.User;
 
 
 
