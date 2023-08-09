952d22ead77fdddff16d7fa7d8a32cbdc44d7df8
eswaribala <parameswaribala@gmail.com>
2023-08-09 11:36:14 +0530

    updated

--- customerapiexternal/src/main/java/com/boa/customerapiexternal/CustomerApiExternalApplication.java
+++ customerapiexternal/src/main/java/com/boa/customerapiexternal/CustomerApiExternalApplication.java
@@ -2,8 +2,11 @@ package com.boa.customerapiexternal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerApiExternalApplication {
 
     public static void main(String[] args) {
