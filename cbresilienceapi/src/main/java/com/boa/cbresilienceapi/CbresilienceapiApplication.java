f18d7a5ae9863ecd8eff8f332493284ad7916764
eswaribala <parameswaribala@gmail.com>
2023-08-10 10:57:51 +0530

    updated

---
+++ cbresilienceapi/src/main/java/com/boa/cbresilienceapi/CbresilienceapiApplication.java
@@ -0,0 +1,13 @@
+package com.boa.cbresilienceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbresilienceapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbresilienceapiApplication.class, args);
+    }
+
+}
---
+++ cbresilienceapi/src/test/java/com/boa/cbresilienceapi/CbresilienceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boa.cbresilienceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbresilienceapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
